60fbdff682cb06ee79f7734b44e35c86c90ebf0f
angcyo <angcyo@126.com>
2015-09-02 11:09:51 +0800

    first commit

--- app/src/main/java/com/angcyo/y2havefun/util/Msg.java
+++
@@ -1,9 +0,0 @@
-package com.angcyo.y2havefun.util;
-
-/**
- * Created by angcyo on 15-08-25-025.
- */
-public class Msg {
-    public static int SMS_VERITY_FAILED = 0x0001;//短信验证失败
-    public static int SMS_VERITY_SUCCEED = 0x0002;//短信验证成功
-}
